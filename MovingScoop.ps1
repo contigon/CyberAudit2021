f7ede0e6446bc8c9fd2aa1a7c5cb4f20a9b0761c
maros17 <72386027+maros17@users.noreply.github.com>
2021-08-09 15:21:33 +0300

    Fining the regex of the shims, added *.cmd to the changed files, and little bit cosmetics of the write-hos menu

--- MovingScoop.ps1
+++ MovingScoop.ps1
@@ -1,7 +1,7 @@
 
 <#
-    Configures scoop and its installed apps to be known by Windows
-    Scoop directory needs to be place in ".\Tools\Scoop\apps\scoop"
+    Configures scoop and its installed apps to be known by Windows, if they not installed in regular way
+    Scoop directory needs to be placed in ".\Tools\Scoop\apps\scoop"
     If you want more tools to be known they and their shims have to be in ".\Tools\Scoop\apps\scoop"
         or in ".\Tools\GlobalScoopApps" if they are global
 #>
@@ -11,48 +11,74 @@ Import-Module $PSScriptRoot\CyberFunctions.psm1
 function updateShims {
     for ($i = 0; $i -lt 2; $i++) {
         if ($i -eq 0) {
-            $shimsPath = "$tools\GlobalScoopApps\shims"
+            $appsRelativePath = "\GlobalScoopApps"
+            $whatShims = "global"
         } else {
-            $shimsPath = "$tools\Scoop\shims"
+            $appsRelativePath = "\Scoop"
+            $whatShims = "local"
         }
-        Get-ChildItem $shimsPath | ForEach-Object {
-            write-host $_.FullName -ForegroundColor DarkMagenta
-            if ($_.Name -like '*.shim') {
-                $file = $_.FullName
-                $regex = '.:\\.*Tools'
-                (Get-Content $file) -replace $regex, $tools | Set-Content $file
-            }
-        }    
-    }   
+        $shimsPath = "$tools$appsRelativePath\shims"
+        $shimsToUpdate = Get-ChildItem -Path $shimsPath\* -Include *.shim, *.cmd 
+        if ($shimsToUpdate.Length -eq 0) {
+            Write-Host "No $whatShims sihms found" -ForegroundColor Red
+        } else {            
+            Write-Host "Updating $whatShims shims:"
+            foreach ($item in $shimsToUpdate) {
+                if (($item.Name -like '*.shim') -or ($item.Name -like '*.cmd')) {
+                    write-host $item.FullName -ForegroundColor DarkMagenta
+                    $file = $item.FullName
+                    $regex = '.:\\.*Tools\\(GlobalScoopApps|Scoop)'
+                    (Get-Content $file) -replace $regex, "$tools$appsRelativePath" | Set-Content $file
+                }
+            }    
+        }   
+    }
+    read-host "Press ENTER to continue" 
+    Clear-Host
 }
-
 function setVars {
     $global:scoopDir = "$tools\Scoop"
     $env:SCOOP_GLOBAL = "$tools\GlobalScoopApps"
     [Environment]::SetEnvironmentVariable("SCOOP_GLOBAL", $env:SCOOP_GLOBAL, "Machine")
     $env:SCOOP = $scoopDir
     [Environment]::SetEnvironmentVariable("SCOOP", $env:SCOOP, "MACHINE")    
+
+    Write-Host "Done. All veriables are set to the current absolute path:" -ForegroundColor Yellow
+    Write-Host "$PSScriptRoot" -ForegroundColor DarkYellow
+    read-host "Press ENTER to continue" 
+    Clear-Host
 }
-function ensure {    
+function ensureInPath {    
     ensure_in_path $env:SCOOP_GLOBAL\shims $true
     ensure_in_path $env:SCOOP\shims
+    read-host "Press ENTER to continue" 
+    Clear-Host
 }
-$tools = "$PSScriptRoot\Tools"
 
-do {
+$tools = "$PSScriptRoot\Tools"
+[Int] $userInput = 0
+while ($userinput -ne 99) {
     $help = @"
-        The options - it's recommended to do them on by one by order:
-        1 - set environments variables
-        2 - update shims
-        3 - ensure scoop and scoop_global in PATH
+  
+        Fix scoop and its tools if you copied them from another computer,
+        and want them be accessible by CMD and Powershell
+
+        Baseline folder is $PSScriptroot 
+
+        1. Variables        | Set environments variables
+        2. Shims            | Update all shims to the current absolute address of Tool
+        3. Ensure in PATH   | ensure scoop and scoop_global in PATH
+        
+        - it's recommended to all commands one by one by order
+
 "@
-    Write-Host $help
-    $userInput = read-host "Choose an action: "
+    Write-Host $help -ForegroundColor Yellow
+    $userInput = read-host "Choose an action"
     switch ($userInput) {
         1 { setVars }
         2 { updateShims }
-        3 { ensure }
+        3 { ensureInPath }
     }
-
-}while ($userinput -ne 99) 
+    
+} 
     
