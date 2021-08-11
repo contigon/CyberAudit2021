395344f7ade74437038bc56d51837d805a35a8a9
maros17 <72386027+maros17@users.noreply.github.com>
2021-08-11 18:31:04 +0300

    Added a 7z SA zip for the new Import-Scoop function

--- MovingScoop.ps1
+++ MovingScoop.ps1
@@ -5,10 +5,17 @@
     If you want more tools to be known they and their shims have to be in ".\Tools\Scoop\apps\scoop"
         or in ".\Tools\GlobalScoopApps" if they are global
 #>
-. "$psscriptroot\Tools\Scoop\apps\scoop\current\lib\core.ps1"
-. "$psscriptroot\Tools\Scoop\apps\scoop\current\lib\install.ps1"
+$tools = "$PSScriptRoot\Tools"
+try {
+    . "$psscriptroot\Tools\Scoop\apps\scoop\current\lib\core.ps1"
+    . "$psscriptroot\Tools\Scoop\apps\scoop\current\lib\install.ps1"
+} catch {
+    Write-Host "Scoop was not found where it supposed to be" -ForegroundColor Red
+    Write-Host "Please place scoop folder in " -ForegroundColor Red -NoNewline
+    Write-Host "$psscriptroot\Tools\Scoop\apps\" -ForegroundColor Yellow
+}
 Import-Module $PSScriptRoot\CyberFunctions.psm1
-function updateShims {
+function Update-Shims {
     for ($i = 0; $i -lt 2; $i++) {
         if ($i -eq 0) {
             $appsRelativePath = "\GlobalScoopApps"
@@ -37,7 +44,7 @@ function updateShims {
     read-host "Press ENTER to continue" 
     Clear-Host
 }
-function setVars {
+function Set-Vars {
     $global:scoopDir = "$tools\Scoop"
     $env:SCOOP_GLOBAL = "$tools\GlobalScoopApps"
     [Environment]::SetEnvironmentVariable("SCOOP_GLOBAL", $env:SCOOP_GLOBAL, "Machine")
@@ -49,14 +56,43 @@ function setVars {
     read-host "Press ENTER to continue" 
     Clear-Host
 }
-function ensureInPath {    
+function Register-Path {    
     ensure_in_path $env:SCOOP_GLOBAL\shims $true
     ensure_in_path $env:SCOOP\shims
+    Read-Host "Press ENTER to continue" 
+    Clear-Host
+}
+function Import-Scoop {
+    #Expand-Archive -Path "$PSScriptRoot\7z.zip" -DestinationPath "$PSScriptRoot"
+    $compressedFilePath = Get-FileName "tar.xz"
+    $compressedFileParentPath = Split-Path $compressedFilePath
+    try {
+        if (!(Test-Path $compressedFilePath) ) {
+            throw "ERROR: File path is not vaild"
+        }
+    } catch {
+        write-host "EEERRRROOORR"
+        return
+    }
+    $7z = "$PSScriptRoot\7z\x64\7za.exe"
+
+    $cmd = "$7z x $compressedFilePath  -o$compressedFileParentPath -txz"
+    Invoke-Expression $cmd
+    $cmd = "$7z x $compressedFileParentPath\Tools.tar -o$psscriptroot\YaaniTools -ttar -aos"
+    Invoke-Expression $cmd
+
+    <#TODO: Finish the function :
+    - Add more bdikot mikrei katze
+    - Change from YaaniTools to Tools after the tests
+    - Check if the chosen compressed file is realy contains a Tool Directory
+    #>
+    #TODO: Test the function with a real Tools directory
+    #TODO: Add a function to compress the Tools directory of an existing CAT and scoop with installed programs
+    
     read-host "Press ENTER to continue" 
     Clear-Host
 }
 
-$tools = "$PSScriptRoot\Tools"
 [Int] $userInput = 0
 while ($userinput -ne 99) {
     $help = @"
@@ -69,6 +105,8 @@ while ($userinput -ne 99) {
         1. Variables        | Set environments variables
         2. Shims            | Update all shims to the current absolute address of Tool
         3. Ensure in PATH   | ensure scoop and scoop_global in PATH
+        4. Import Scoop     | Import an existing Tools.tar.xz that contains scoop with programs
+        5. Export Scoop     | Export the installed programs to a tar.xz file in order to import them in another computer
         
         - it's recommended to run all commands one by one by order
 
@@ -76,11 +114,15 @@ while ($userinput -ne 99) {
     Write-Host $help -ForegroundColor Yellow
     $userInput = read-host "Choose an action"
     switch ($userInput) {
-        1 { setVars }
-        2 { updateShims }
-        3 { ensureInPath }
+        1 { Set-Vars }
+        2 { Update-Shims }
+        3 { Register-Path }
+        4 { Import-Scoop }
     }
-        #TODO: add an option to compress all of the apps, and option to extract them to the right place
+    #TODO: Improve the ugly menu
+    #TODO: Implement an Export-Scoop function
+
+    #TODO: add an option to compress all of the apps, and option to extract them to the right place
     <#
     #$7z = "C:\Users\Me\Desktop\7z\7za.exe"
 $7z = "7z"
